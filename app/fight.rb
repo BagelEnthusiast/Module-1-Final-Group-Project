c6f8f510227700e0eee891c35a3f359df16db078
BagelEnthusiast <nathan.richardson94@gmail.com>
2019-04-17 15:34:03 -0500

    fight update

--- app/fight.rb
+++ app/fight.rb
@@ -1,39 +1,21 @@
 def fight(party_member, opponent)
-    
-    while party_member.temp_health > 0 || opponent.temp_health > 0
+  while party_member.temp_health > 0 && opponent.temp_health > 0
     damage_calc(party_member, opponent)
-        if opponent.temp_health > 0
-            damage_calc(opponent, party_member)
-        end
-
-        #battle_choice(party_member, opponent)
+    if opponent.temp_health > 0
+      damage_calc(opponent, party_member)
     end
 
-    battle_win_menu(party_member, opponent) if party_member.temp_health > opponent.temp_health
-    battle_lose_menu(party_member, opponent) if party_member.temp_health < opponent.temp_health
+    battle_choice(party_member, opponent) if party_member.temp_health > 0
+  end
 
+  battle_win_menu(party_member, opponent) if party_member.temp_health > opponent.temp_health
+  battle_lose_menu(party_member, opponent) if party_member.temp_health < opponent.temp_health
 end
 
-def attack
-    $prompt.select("Choose your action") do |menu|
-        menu.choice "Attack"
-        menu.choice "Run"
-      end
-end
-
-# def attack_choice
-#     case attack
-#     when
-#     when
-
-# end
-
-# def enemy_attack
-
-# end
 
 def damage_calc(attacker, defender)
-    damage = attacker.attack * (10/defender.defense)
-    defender.temp_health -= damage
-    puts "#{attacker.name} attacked #{defender.name} for #{damage} damage."
+  damage = attacker.attack.to_f * (10.0 / defender.defense.to_f)
+  binding.pry
+  defender.temp_health -= damage
+  puts "#{attacker.name} attacked #{defender.name} for #{damage} damage."
 end
